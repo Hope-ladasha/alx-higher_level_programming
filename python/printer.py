573db31ce33c397c6d7f4aa73973d4e7ecc1df2a
Hope-Ladasha <brianbaliach@Brians-MacBook-Pro.local>
2023-11-29 12:51:49 +0300

    .gitignore fix

---
+++ python/printer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def printer(string):
+    print(string)
+
+
+if __name__ == "__main__":
+    printer("100, 56")
---
+++ python/subtraction.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def subtraction(a, b):
+    return a - b
+
+
+if __name__ == "__main__":
+    print(subtraction(100, 56))
